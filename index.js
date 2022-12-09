475d1b01f0a2c63d646b32a2e9576b8a071b7f05
Nego <61694826+kardespro@users.noreply.github.com>
2022-12-09 09:14:16 +0400

    Update index.js
    HeartBeat

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ const BASE = "192.168.16.1"
     === RedLine Wifi Router Stats ===
   `)
   const _ = await axios.get(`${BASE}/goform/get_router_status`)
+  const __ = await axios.get(`${BASE}/goform/webheartbeat`);
   console.log(`
   =============
    UpLoad Speed : ${_.data.upspeed}
    Download Speed : ${_.data.downspeed}
    Terminal ID : ${_.data.ternum}
+   HeartBeart : ${__.data.result}
   =============
   `)
 })()
